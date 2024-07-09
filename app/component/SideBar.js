58b14e0922857718319bb22c6fc6f9b59c10a23f
athrey-navda <athu.navada@gmail.com>
2024-07-09 02:36:14 -0400

    update: remove settings link

--- app/component/SideBar.js
+++ app/component/SideBar.js
@@ -62,7 +62,7 @@ export default function Sidebar() {
     },
     {
       name: "Settings",
-      link: "/settings",
+      link: "/",
       icon: MdOutlineSettings,
       current: currentNav === "Settings",
     },
